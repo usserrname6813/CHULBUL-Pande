c257cf43efca433bd49812cc490bde81fb909dbb
wajeeh <125567795+usserrname6813@users.noreply.github.com>
2025-01-03 06:06:38 +0500

    Update chariya.py

--- chariya.py
+++ chariya.py
@@ -3,6 +3,8 @@ from BlaApi import Client
 from html2text import html2text as h2t
 from markdownify import markdownify
 from time import sleep
+from flask import Flask
+from threading import Thread
 
 # Discord webhook URL
 webhook_url = "https://discord.com/api/webhooks/1318358536261992530/XlT3IzvrVthRB5jqYsZxSVQfPBzEV9M93LWp3D_kQqe7CzNgM-Yj8uGwLUOTue7x7sGQ"
@@ -10,6 +12,17 @@ webhook_url = "https://discord.com/api/webhooks/1318358536261992530/XlT3IzvrVthR
 # School app login credentials
 username = '03452101562'
 password = 'wajeeh6813'
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "The bot is running!"
+
+def keep_alive():
+    """Starts a thread to run a Flask server."""
+    t = Thread(target=lambda: app.run(host='0.0.0.0', port=8080))
+    t.start()
 
 def send_to_discord(message):
     """Sends a message to the Discord channel using the provided webhook URL."""
@@ -47,18 +60,6 @@ def main():
 
     previous_diaries = []  # Store previously sent diary IDs
 
-    # Get initial 2 homework entries (optional)
-    # You can uncomment this block to send the first 2 entries
-    # upon startup
-    # date = c.get_current_date()
-    # initial_diaries = c.search_by_student(student_id=student_id)
-    # initial_diaries = c.search_by_date(date, passthru=initial_diaries) 
-    # for diary in initial_diaries[:2]:  # Get the first 2 diaries
-    #     previous_diaries.append(diary['notificationId'])
-    #     formatted_data = format_diary([diary])
-    #     for message in formatted_data:
-    #         send_to_discord(message)
-
     while True:
         try:
             date = c.get_current_date()
@@ -82,4 +83,5 @@ def main():
         sleep(60)  # Wait for 60 seconds before checking for new diaries
 
 if __name__ == "__main__":
+    keep_alive()  # Start the web server
     main()
